d47829584d0cf7bb73d12c05260e22d78f111df6
giaco <giacomo.nasi@gmail.com>
2024-07-19 15:10:12 +0200

    Added logs if the first argument is 'log'

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime
+import sys
+from os import system
 from time import sleep
 
 import keyboard
@@ -7,12 +9,15 @@ import pyperclip
 pressed_keys = set()
 
 
-def on_key_event(event):
+def on_key_event(event, log_key_pressed=False):
     global pressed_keys
     if event.event_type == 'down':
         pressed_keys.add(event.name)
     if event.event_type == 'up':
         pressed_keys.discard(event.name)
+
+    if log_key_pressed:
+        print(pressed_keys or {})
 
     if pressed_keys == {'ctrl', 'alt gr', 'alt'}:
         with open('clipboard.txt', 'r') as fd:
@@ -23,7 +28,12 @@ def on_key_event(event):
         print(f'{str(datetime.datetime.now())} - Clipboard updated')
 
 
-keyboard.hook(on_key_event)
+if __name__ == '__main__':
+    try:
+        log = sys.argv[1] == 'log'
+    except IndexError:
+        log = False
+    keyboard.hook(lambda event: on_key_event(event, log_key_pressed=log))
 
-while True:
-    sleep(3600)
+    while True:
+        sleep(3600)
